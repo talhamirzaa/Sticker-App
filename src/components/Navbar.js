b96d7582bf595f61148b0798b0c8c106e58461f6
talhamirzaa <mirzatalha0705@gmail.com>
2024-09-22 21:52:44 +0530

    sit name styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:'rgb(77, 16, 77)'}}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/" id="nvnm">Stickerz</Link>
+    <Link className="navbar-brand fst-italic" to="/" id="nvnm">Stickerz</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
